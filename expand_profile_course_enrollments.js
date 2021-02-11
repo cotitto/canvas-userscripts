64b9076d22f755588120c9e33a73252bc58ae0e3
Xavier <cotitto@users.noreply.github.com>
2021-02-11 17:57:55 -0500

    Update expand_profile_course_enrollments.js

--- expand_profile_course_enrollments.js
+++ expand_profile_course_enrollments.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Expand Profile Course Enrollment List while in Profile
 // @namespace    https://github.com/cotitto
-// @version      1.1
+// @version      1
 // @include      https://*.instructure.com/accounts/1/users/*
 // @description  Expand course enrollments when visiting profile
 // @author       cotitto
