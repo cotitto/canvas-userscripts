3e15c2782289659a38d11077d156af22b0134af1
Xavier <20861475+cotitto@users.noreply.github.com>
2021-09-15 11:15:31 -0500

    Create Static Course Menu.js

---
+++ Static Course Menu.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Static Course Menu
+// @namespace    https://github.com/cotitto/canvas-userscripts
+// @version      1.1
+// @description  Static Course Menu
+// @author       XCT
+// @include      https://*.instructure.com/courses/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('.with-left-side, #left-side').css('position', 'sticky')
+    $('.ic-Layout-contentMain').css('margin-top', '-1140px')
+    $('#left-side').css({"padding-top": "20px", "margin-left": "-200px"});
+})();
